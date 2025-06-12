9eafaa2d040e283b9f4fa9c3d400bd9950c53ab5
DucPPhan <phanminhduc23@gmail.com>
2025-06-12 18:12:56 +0700

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -90,7 +90,7 @@ const productSchema = new mongoose.Schema(
     },
     subcategory: {
       type: String,
-      required: true,
+      // required: true,
     },
     isPopular: {
       type: Boolean,
